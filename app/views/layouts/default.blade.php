4952d6057d76abab911d212883ce3b016f056b07
MrMM1989 <MrMM1989@users.noreply.github.com>
2014-11-08 21:04:03 +0100

    css files added with basic css

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -3,8 +3,14 @@
   <head>
     <meta charset="utf-8">
     <title>{{ $title }}</title>
+	{{ HTML::style('css/reset.css') }}
+	{{ HTML::style('css/main.css') }}
   </head>
   <body>
-    @yield('content')
+	<!-- start of div sitebody -->
+	<div id="sitebody">
+    	@yield('content')
+	</div>
+	<!-- end of div sitebody -->
   </body>
 </html>
